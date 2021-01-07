671e63364594d5fc741d4216054a1bd8324a79c4
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-01-07 11:41:16 +0000

    Move Loading comditional to App

--- app/components/Stories.js
+++ app/components/Stories.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import Loading from './Loading'
 
-export default function Stories(props) {
+export default function Stories({ stories }) {
+  return stories.map((story) => {
+    const date = new Date(story.time * 1000).toLocaleString()
 
-  if (props.stories[props.type]) {
-    return props.stories[props.type].map((story) => {
-      return (
-        <li key={story.id}>
-          <a href={story.url}>{story.title}</a>
-          <div>
-            <div>by {story.by} on DATE with {story.descendants} {story.descendants === 1 ? 'comment' : 'comments'}</div>
-          </div>
-        </li>
-      )
-    })
-  } else {
-    return <Loading />
-  }
+    return (
+      <li key={story.id}>
+        <a href={story.url}>{story.title}</a>
+        <div>
+          <div>by {story.by} on {date} with {story.descendants} {story.descendants === 1 ? 'comment' : 'comments'}</div>
+        </div>
+      </li>
+    )
+  })
 }
